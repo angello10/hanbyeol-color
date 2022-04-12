82f5600bf7af148471961691da139d3efdb0c021
angello10 <angello10@naver.com>
2022-04-12 22:40:15 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# hanbyeol-color
+![coin](./img/coin.png)
+한별포스 + 잔디 뽑기 2연타로 탕진했습니다
+![hanbyeol](./img/hanbyeol.png)
+아쉬운 대로 기분이라도 내보려고 합니다
+## 사용법
+1. solved.ac에서 프로필 페이지에 들어갑니다
+2. F12키를 누르고 console에 들어갑니다
+3. 코드를 붙여넣고 엔터키를 눌러서 실행합니다
---
+++ test.js
@@ -0,0 +1,25 @@
+var arr = document.querySelectorAll("#__next > div > div > div > div > div > svg > rect");
+
+var origin = [
+	arr[arr.length-5].getAttribute("fill"),
+	arr[arr.length-4].getAttribute("fill"),
+	arr[arr.length-3].getAttribute("fill"),
+	arr[arr.length-2].getAttribute("fill"),
+];
+
+var converted = [
+	"rgb(255, 212, 89)",
+	"rgb(255, 170, 105)",
+	"rgb(255, 124, 121)",
+	"rgb(255, 95, 132)",
+];
+
+var t = 0;
+setInterval(() => {
+	t++;
+	arr.forEach(p => {
+		var i = origin.indexOf(p.getAttribute("fill"));
+		if (i != -1) p.setAttribute("fill", converted[(i+t)%4]);
+	});
+	for (var i = 0; i < 4; i++) origin[i] = converted[(i+t)%4];
+}, 100);
